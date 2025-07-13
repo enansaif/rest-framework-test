a389c8884f343b1004943de7db11d24280782c0f
enansaif <enansaifme33@gmail.com>
2025-07-13 14:07:36 +0600

    corrected some linting issues

--- app/core/models.py
+++ app/core/models.py
@@ -20,7 +20,7 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
-    
+
     def create_superuser(self, email, password=None, **kwargs):
         user = self.create_user(email, password, **kwargs)
         user.is_staff = True
