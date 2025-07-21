c3f8c5b7fdaea3a07ff0ce1839125b8183259458
enansaif <enansaifme33@gmail.com>
2025-07-21 15:35:54 +0600

    model changes to add images

--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -4,6 +4,7 @@ Test for django models.
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from core import models
+from unittest.mock import patch
 
 
 def create_user(*args, **kwargs):
@@ -87,3 +88,10 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'example.jpg')
+        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
